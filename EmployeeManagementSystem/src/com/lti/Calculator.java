05105c9c5b4578a29fecc2417c0f545ede6fa3c5
krishnv <krishna.vats02@gmail.com>
2020-08-07 16:30:58 +0530

    Update Calculator.java
    Calc completed

--- EmployeeManagementSystem/src/com/lti/Calculator.java
+++ EmployeeManagementSystem/src/com/lti/Calculator.java
@@ -2,16 +2,24 @@ package com.lti;
 
 public class Calculator {
 
-	public static void add(int a, int b){
-		System.out.println("Sum = "+(a+b));
-	}
-	public static void diff(int a, int b){
-		System.out.println("diff = "+(a-b));
-	}
-	
-	
-	public static void main(String[] args) {
-		add(10,30);
-	}
+  public static int add(int a,int b){
+    return a+b;
+  }
+  public static int sub(int a,int b){
+    return a-b;
+  }
+  public static int mul(int a,int b){
+    return a*b;
+  }
+  public static int div(int a,int b){
+    return a/b;
+  }
+  public static void main(String []args){
+    System.out.println(add(10,20));
+    System.out.println(sub(10,20));
+    System.out.println(mul(10,20));
+    System.out.println(div(10,20));                       
+  }
+
 
 }
